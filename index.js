d5dfa46e000d33fcd1cd34d5037988506a953d09
Alexander Mekhonoshin (invntrm) <invntrm@yandex-team.ru>
2016-11-24 02:28:37 +0300

    0.0.1

--- index.js
+++ index.js
@@ -1,21 +1,69 @@
+const $$ = (document, selector) => document.querySelectorAll(selector)
+const $ = require('ramda')
+    const map = $.map
+    const merge = $.merge
+    const range = $.range
+        const range1 = range(1)
+    const unnest = $.unnest
 const process = require('process')
-const tableBuilder = require('table-builder')
-    const table = tableBuilder({class: 'avito'})
-    const headers = {price:'price', title:'title'}
+const getopt = require('meow')
+    const opts = getopt(`
+    Usage
+      $ ${process.argv[1]} <uri>
+
+    Options
+      --html-table, -t  Output html table representation
+      --page-count, -n  Count of the pages to crawl
+    `)
+    getopt.uri = getopt.input
+    const uri = getopt.uri || 'https://www.avito.ru/moskva/telefony/iphone?bt=1&q=iphone+se'
+        const getPageUri = page => `${uri}&p=${page}`
+    const isHtmlTable = opts.flags.htmlTable
+const TableBuilder = require('table-builder')
+    const table = new TableBuilder({class: 'avito'})
+    const headers = {price: 'Price', title: 'Title', mem: 'Storage', color: 'Color', uri: 'Uri'}
+    const leftpad3 = val => require('leftpad')(val, 3)
+    const htmlTable = data => table
+        .setHeaders(headers)
+        .setPrism('uri', uri => `<a href="${uri}">link</a>`)
+        .setPrism('price', p => `${parseInt(p / 1000)} ${leftpad3(p % 1000)}`)
+        .setPrism('mem', m => m || '—')
+        .setPrism('color', c => c.toLowerCase() || '—')
+        .setData(data)
+        .render()
 const thrw = require('throw')
 const fetch = require('isomorphic-fetch')
-    const getHttp = (uri) => fetch.then(r => r.status >= 400 ? thrw (r.status) : r.body)
-const parseHtml = html => require('parse5').parse(html)
+    const getHttp = uri => fetch(uri).then(r => r.status >= 400 ? thrw (r.status) : r.text())
+const parseHtml = html => require('jsdom').jsdom(html)
+const Uri = require('urijs')
+    const hostname = (uri, hostname) => hostname ? Uri(uri).hostname(hostname).toString() : Uri(uri).hostname()
 
-const uri = process.argv[2] || 'https://www.avito.ru/moskva/telefony/iphone?q=iphone+se'
+const retreiveData = document =>
+    Array.from($$(document, '.js-catalog_after-ads .item'))
+    .map(i => map(p=>i.querySelector(p), {title: '.title', price: '.about', uri: '.item-description-title-link'}))
+    .map(i => merge(i, {uri: i.uri.getAttribute('href')}))
+    .map(i => map(t => typeof t === 'string' ? t : t.textContent.trim(), i))
 
-const retreiveData = (document) => Array.from(document.querySelectorAll('.js-catalog_after-ads .item')).map(i=>({title:i.querySelector('.title'), price:i.querySelector('.about')})).map(({title,price})=>({title:title.textContent.trim(),price:price.textContent.trim()}))
+const processData = data => data
+    .map(i => (merge(i, {price: +i.price.replace(/\s+|руб\./g, '')})))
+    .filter(i => i.title.match(/se/i))
+    .map(i => i.title.match(/A1723/i) ? merge(i, {a1723:true}) : i)
+    .map(i => merge(i, {title: i.title.replace(/(iPhone|Apple|айфон)\s?/ig, '')}))
+    .filter(i => !i.title.match(/\b(6|7|5S|Samsung|7plus|6plus)\b/ig))
+    .sort((i, j) => i.price < j.price ? 1 : -1)
+    .map(i => merge(i, {mem: +(i.title.match(/64|32|16/) || [NaN])[0]}))
+    .map(i => merge(i, {color: (i.title.match(/rosegold|gold|pink|silver|black|розовый|золотой|серый|серебристый|ч[ёе]рный/i) || [''])[0]}))
+    .map(i => merge(i, {uri: i.uri.match(/^(https?:)?\/\//) ? i.uri : 'https:' + hostname(i.uri, hostname(uri))}))
 
-const main = async function () {
-    const html = await getHttp(uri)
-    const document = await parseHtml(html)
-    const data = retreiveData(document)
-
-    return table.setHeaders(headers).setData(data).render()
+const htmlOutput = data => {
+    const style = `<style>body{text-align:center;}.avito{width:100%;}thead{text-align:left;}.price-td{text-align:right;}</style>`
+    return htmlTable(data)
 }
-console.log(main())
+
+const main = () =>
+    Promise.all(range1(opts.flags.pageCount).map(i => getHttp(getPageUri(i)).then(parseHtml).then(retreiveData)))
+    .then(dataArr => unnest(dataArr))
+    .then(processData)
+    .then(data => isHtmlTable ? htmlOutput(data) : JSON.stringify(data, null, 4))
+
+main().then(r=>{console.log(r); process.exit(0)})
